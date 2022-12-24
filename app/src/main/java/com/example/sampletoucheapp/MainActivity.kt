c9691d42169b2c8fb82176078fbf52c2da6a4988
Rin-t <rin.t.4921@gmail.com>
2022-12-24 21:50:58 +0900

    ボタンのタップ処理を追加

--- app/src/main/java/com/example/sampletoucheapp/MainActivity.kt
+++ app/src/main/java/com/example/sampletoucheapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.sampletoucheapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textView)
+        val dogButton: Button = findViewById(R.id.buttonDog)
+        val catButton: Button = findViewById(R.id.buttonCat)
+        val clearButton: Button = findViewById(R.id.buttonClear)
+
+        dogButton.setOnClickListener {
+            textView.text = "いぬ"
+        }
+
+        catButton.setOnClickListener {
+            textView.text = "ねこ"
+        }
+
+        clearButton.setOnClickListener {
+            textView.text = ""
+        }
+
     }
 }
